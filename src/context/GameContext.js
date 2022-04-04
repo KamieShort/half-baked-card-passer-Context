b35ea8463907800964ecefeda5f407d16cc6af5e
Kamie Short <kamie.short@gmail.com>
2022-04-04 15:53:09 -0700

    set up gameObject

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -5,7 +5,37 @@ const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
   const [deck, setDeck] = useState(initialCards);
-  return <GameContext.Provider value={{ deck, setDeck }}>{children}</GameContext.Provider>;
+  const [playerOneHand, setPlayerOneHand] = useState([]);
+  const [selectedCard, setSelectedCard] = useState();
+  const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  const [playerThreeHand, setPlayerThreeHand] = useState([]);
+  const [from, setFrom] = useState('deck');
+  const [to, setTo] = useState(1);
+
+  const gameObject = {
+    deck,
+    setDeck,
+    playerOneHand,
+    setPlayerOneHand,
+    selectedCard,
+    setSelectedCard,
+    playerTwoHand,
+    setPlayerTwoHand,
+    playerThreeHand,
+    setPlayerThreeHand,
+    from,
+    setFrom,
+    to,
+    setTo,
+  };
+
+  return <GameContext.Provider value={{ gameObject }}>{children}</GameContext.Provider>;
 };
 
-export { GameProvider };
+const useGameContext = () => {
+  const context = useContext(GameContext);
+
+  return context;
+};
+
+export { GameProvider, useGameContext };
