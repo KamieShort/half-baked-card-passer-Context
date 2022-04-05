157250374d39b1d49f0749fe4236686d85d911a1
Kamie Short <kamie.short@gmail.com>
2022-04-04 17:32:56 -0700

    removed duplicate props on app page

--- src/App.js
+++ src/App.js
@@ -11,59 +11,43 @@ function App() {
   const [selectedCard, setSelectedCard] = useState();
   const [playerTwoHand, setPlayerTwoHand] = useState([]);
   const [playerThreeHand, setPlayerThreeHand] = useState([]);
-  const [from, setFrom] = useState('deck');
-  const [to, setTo] = useState(1);
+  // const [from, setFrom] = useState('deck');
+  // const [to, setTo] = useState(1);
 
   return (
     <div className="App">
       <section>
         {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
         <Player
-          to={to}
+          // to={to}
           player={1}
           hand={playerOneHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
+          // setFrom={setFrom}
+          // selectedCard={selectedCard}
+          // // setTo={setTo}
+          // setSelectedCard={setSelectedCard}
         />
         <Player
-          to={to}
+          // to={to}
           player={2}
           hand={playerTwoHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
+          // setFrom={setFrom}
+          // selectedCard={selectedCard}
+          // setTo={setTo}
+          // setSelectedCard={setSelectedCard}
         />
         <Player
-          to={to}
+          // to={to}
           player={3}
           hand={playerThreeHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
+          // setFrom={setFrom}
+          // selectedCard={selectedCard}
+          // setTo={setTo}
+          // setSelectedCard={setSelectedCard}
         />
-        <CardList
-          cards={deck}
-          selectedCard={selectedCard}
-          setSelectedCard={setSelectedCard}
-          setFrom={setFrom}
-          player={'deck'}
-        />
+        <CardList cards={deck} player={'deck'} />
       </section>
-      <section>
-        {selectedCard && (
-          <ExecutePassButton
-            setFrom={setFrom}
-            from={from}
-            to={to}
-            selectedCard={selectedCard}
-            setSelectedCard={setSelectedCard}
-          />
-        )}
-      </section>
+      <section>{selectedCard && <ExecutePassButton />}</section>
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { useGameContext } from '../context/GameContext';
 import CardList from './CardList';
 
-export default function Player({
-  player,
-
-  hand,
-}) {
-  const { setTo, setFrom, setSelectedCard, selectedCard, to } = useGameContext();
+export default function Player({ player, hand }) {
+  const { setSelectedCard, selectedCard, to, setTo, setFrom } = useGameContext();
   return (
     <div
       className={`player ${to === player ? 'selected-player' : ''}`}
